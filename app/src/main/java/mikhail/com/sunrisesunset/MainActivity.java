454d33b2d027859db685544f8e142416f2ebcb92
Michael B <michaelbor2015@gmail.com>
2016-07-07 23:12:49 -0700

    added butterknife

--- app/src/main/java/mikhail/com/sunrisesunset/MainActivity.java
+++ app/src/main/java/mikhail/com/sunrisesunset/MainActivity.java
@@ -2,12 +2,26 @@ package mikhail.com.sunrisesunset;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.date)
+    TextView mDate;
+    @BindView(R.id.location)
+    TextView mLocation;
+    @BindView(R.id.sunrise)
+    TextView mSunrise;
+    @BindView(R.id.sunset)
+    TextView mSunset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
