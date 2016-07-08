0cd123286c6f8e7f5565e179aad3f148729e1c63
Michael B <michaelbor2015@gmail.com>
2016-07-07 23:39:17 -0700

    location enabled

--- app/src/main/java/mikhail/com/sunrisesunset/MainActivity.java
+++ app/src/main/java/mikhail/com/sunrisesunset/MainActivity.java
@@ -1,13 +1,27 @@
 package mikhail.com.sunrisesunset;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.luckycatlabs.sunrisesunset.SunriseSunsetCalculator;
+import com.luckycatlabs.sunrisesunset.dto.Location;
+
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    SunriseSunsetCalculator sunriseSunsetCalculator;
 
     @BindView(R.id.date)
     TextView mDate;
@@ -23,5 +37,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        getLocation();
+        getSunriseSunsetCalculator();
+        getSunsetandSunrise();
+    }
+
+    public void getSunriseSunsetCalculator() {
+
+//        LocationManager lm  = (LocationManager) getSystemService(LOCATION_SERVICE);
+//
+//        if(!lm.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+//                !lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+//
+//        }
+        Location location = new Location("39.9522222", "-75.1641667");
+        sunriseSunsetCalculator = new SunriseSunsetCalculator(location, "America/New_York");
+
+
+    }
+
+    public void getSunsetandSunrise() {
+        String officialSunrise = sunriseSunsetCalculator.getOfficialSunriseForDate(Calendar.getInstance());
+        String officialSunset = sunriseSunsetCalculator.getOfficialSunsetForDate(Calendar.getInstance());
+        mSunrise.setText("Sunrise today: " + officialSunrise.toString());
+        mSunset.setText("Sunset today: " + officialSunset);
+//        Log.d("MainActivity","Sunrise" + officialSunrise.toString());
+
+    }
+
+    public void getLocation(){
+
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if(!lm.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                !lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            // Build the alert dialog
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Location Services Not Active");
+            builder.setMessage("Please enable Location Services and GPS");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    // Show location settings when the user acknowledges the alert dialog
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+                }
+            });
+            Dialog alertDialog = builder.create();
+            alertDialog.setCanceledOnTouchOutside(false);
+            alertDialog.show();
+        }
     }
 }
